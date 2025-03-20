b02963f117f0ec6e2506b25a1bab8c02536adca2
hw <hyeongu0305@gmail.com>
2025-03-20 20:56:38 +0900

    버튼 색상 활성화 되도록 수정

--- week1/common.js
+++ week1/common.js
@@ -2,14 +2,26 @@
 function createTabs() {
     const tabsContainer = document.getElementById('tabs');
     const tabs = [
-        { name: 'HTML 기본 태그', content: 'html-basics.html' },
-        { name: 'CSS 기본 태그', content: 'css-basics.html' },
-        { name: 'JavaScript 기초', content: 'js-basics.html' }
+        { id: 'main', name: '메인', content: '../../index.html' },
+        { id: 'basicTags', name: 'HTML 기본 태그', content: '../step1/index.html' },
+        { id: 'cssBasicTags', name: 'CSS 기본 태그', content: 'css-basics.html' },
+        { id: 'javascriptBasic', name: 'JavaScript 기초', content: 'js-basics.html' }
     ];
 
     tabs.forEach(tab => {
         const button = document.createElement('button');
         button.textContent = tab.name;
+
+        if (id != '' && id == tab.id) {
+            button.setAttribute('style', 'background-color: #8cbf92; color: #243b27; font-weight: 500;');
+        } else {
+            button.setAttribute('style', 'font-weight: 200;');
+        }
+
+        if (tab.id == 'main') {
+            button.setAttribute('style', 'background-color: #457d4c; color: #fff; font-weight: 700;');
+        }
+
         button.addEventListener('click', () => {
             window.location.href = tab.content;
         });
