e686b74ecc39d21c8ecde45c59c4c60b7eb73ee5
hw <hyeongu0305@gmail.com>
2025-03-26 12:24:16 +0900

    메인화면용 home 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'folder/:id', loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule) },
   { path: 'html', loadChildren: () => import('./html/html.module').then(m => m.HTMLPageModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
 ];
 
 @NgModule({
