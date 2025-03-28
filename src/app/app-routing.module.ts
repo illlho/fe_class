dfa0369cbf7f8083ea2b6a939a562b7543cf8ee5
hw <hyeongu0305@gmail.com>
2025-03-28 11:41:07 +0900

    기본 리다이렉트 경로 수정

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'folder/:id', loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule) },
   { path: 'html', loadChildren: () => import('./html/html.module').then(m => m.HTMLPageModule) },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
