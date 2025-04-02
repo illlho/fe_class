cb9ad3803bb0d369fe1e234a7cd074d3297283d0
hw <hyeongu0305@gmail.com>
2025-04-02 14:52:03 +0900

    todo 관련 화면 생성

---
+++ src/app/todo/todo-create/todo-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-create',
+  imports: [],
+  templateUrl: './todo-create.component.html',
+  styleUrl: './todo-create.component.css'
+})
+export class TodoCreateComponent {
+
+}
---
+++ src/app/todo/todo-trash/todo-trash.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-trash',
+  imports: [],
+  templateUrl: './todo-trash.component.html',
+  styleUrl: './todo-trash.component.css'
+})
+export class TodoTrashComponent {
+
+}
