a8f28cf3cc2abe8c0060ebcea5944f20f9353460
hw <hyeongu0305@gmail.com>
2025-04-02 11:41:01 +0900

    이전 작업 내용 제거

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'fe_class',
-  webDir: 'www'
-};
-
-export default config;
