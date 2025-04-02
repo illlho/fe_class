929553703cc0771730a1169571afeb43275d915b
hw <hyeongu0305@gmail.com>
2025-04-02 14:51:50 +0900

    스프린트 리스트 화면 생성

---
+++ src/app/sprint-list/sprint-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-sprint-list',
+    imports: [],
+    templateUrl: './sprint-list.component.html',
+    styleUrl: './sprint-list.component.css'
+})
+export class SprintListComponent {
+
+}
