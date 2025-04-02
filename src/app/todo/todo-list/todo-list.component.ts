3474d23a7b25bd04988a6ee906e2d1fed04c316b
hw <hyeongu0305@gmail.com>
2025-04-02 17:27:51 +0900

    리스트 화면 데이터 바인딩 로직 추가

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonService } from '../../common.service';
+import { CommonModule } from '@angular/common';  
 
 @Component({
     selector: 'app-todo-list',
-    imports: [RouterLink],
+    imports: [RouterLink, CommonModule],
     templateUrl: './todo-list.component.html',
     styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
-    constructor(private commonService: CommonService) {}
+    constructor(private commonService: CommonService) { }
+    todoList: any[] = [];
+
+    ngOnInit(): void {
+        this.todoList = this.commonService.getTodoList();
+    }
 
     goBack(): void {
         this.commonService.goBack();
