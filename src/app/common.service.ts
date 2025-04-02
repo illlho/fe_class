c0c6227293a72b659750e5d73e1d38f3aafd7069
hw <hyeongu0305@gmail.com>
2025-04-02 17:10:53 +0900

    todo app 데이터 저장 관련 공용 함수 추가

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -14,4 +14,42 @@ export class CommonService {
         //     this.location.back(); // 뒤로 갈 수 있을 때만 실행
         // }
     }
+
+    setTodoList(todoList: any[]): void {
+        localStorage.setItem('todoList', JSON.stringify(todoList));
+    }
+
+    getAllTodoList(): any[] {
+        return JSON.parse(localStorage.getItem('todoList') || '[]');
+    }
+
+    getTodoList(): any[] {
+        return this.getAllTodoList().filter(item => item.isDeleted === false);
+    }
+
+    getTrashList(): any[] {
+        return this.getAllTodoList().filter(item => item.isDeleted === true);
+    }
+
+    getLastId(): number {
+        const todoList = this.getAllTodoList();
+        return todoList[todoList.length - 1].id;
+    }
+
+    upsertTodo(todo: any): void {
+        let id = todo['id'] || this.getLastId() + 1;
+        let todoList = this.getAllTodoList();
+
+        let todoIndex = todoList.findIndex(item => item.id === id);
+        if (todoIndex > -1) {
+            todoList[todoIndex] = todo;
+        } else {
+            todoList.push({ id, ...todo });
+        }
+        this.setTodoList(todoList);
+    }
+
+    findTodoById(id: number): any {
+        return this.getAllTodoList().find(item => item.id === id);
+    }
 }
