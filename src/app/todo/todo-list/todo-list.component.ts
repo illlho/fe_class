38b614d6cf34d968ce57706a76be410f0b17135f
hw <hyeongu0305@gmail.com>
2025-04-03 17:25:42 +0900

    할 일 수정 기능 개발

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { CommonService } from '../../common.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TodoCreateComponent } from '../todo-create/todo-create.component';
 
 @Component({
     selector: 'app-todo-list',
-    imports: [CommonModule, TodoCreateComponent],
+    imports: [CommonModule, TodoCreateComponent, FormsModule],
     templateUrl: './todo-list.component.html',
     styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
     constructor(private commonService: CommonService) { }
     todoList: any[] = [];
+    editingTodoId: number | null = null;
+    editTodoForm: Partial<any> = {};
 
     ngOnInit(): void {
         this.refreshTodoList();
@@ -31,7 +33,7 @@ export class TodoListComponent {
     createTodo(event: void): void {
         this.refreshTodoList();
     }
-    
+
     changeTodoDone(id: number): void {
         this.commonService.changeTodoDone(id);
         this.refreshTodoList();
@@ -39,5 +41,28 @@ export class TodoListComponent {
 
     refreshTodoList(): void {
         this.todoList = this.commonService.getTodoList();
+    }
+
+    startEdit(todo: any) {
+        this.editingTodoId = todo.id;
+        this.editTodoForm = {
+            title: todo.title,
+            category: todo.category,
+            deadline: todo.deadline
+        };
+    }
+
+    cancelEdit() {
+        this.editingTodoId = null;
+        this.editTodoForm = {};
+    }
+
+    saveTodoEdit(todo: any) {
+        this.commonService.upsertTodo({
+            ...todo,
+            ...this.editTodoForm,
+        });
+        this.cancelEdit();
+        this.refreshTodoList();
     }
 }
