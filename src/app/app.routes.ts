3a80f7f239a40ef5307c02cc71ad01442e169b06
hw <hyeongu0305@gmail.com>
2025-04-04 18:14:19 +0900

    미사용 컴포넌트 (trash) 제거

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { SprintListComponent } from './sprint-list/sprint-list.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { TodoCreateComponent } from './todo/todo-create/todo-create.component';
 import { TodoDetailComponent } from './todo/todo-detail/todo-detail.component';
-import { TodoTrashComponent } from './todo/todo-trash/todo-trash.component';
 
 export const routes: Routes = [
     { path: '', component: SprintListComponent },
     { path: 'todo-list', component: TodoListComponent },
     { path: 'todo-create', component: TodoCreateComponent },
     { path: 'todo-detail', component: TodoDetailComponent },
-    { path: 'todo-trash', component: TodoTrashComponent },
 ];
