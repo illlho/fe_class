5f9fc4dfba6b3a1db38a381cffa49bc2a99f902d
hw <hyeongu0305@gmail.com>
2025-04-04 16:50:49 +0900

    sprint list 화면 ui 수정 및 반복문으로 화면 구현하도록 수정

--- src/app/sprint-list/sprint-list.component.ts
+++ src/app/sprint-list/sprint-list.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-sprint-list',
-    imports: [RouterLink],
+    imports: [RouterLink, CommonModule],
     templateUrl: './sprint-list.component.html',
-    styleUrl: './sprint-list.component.css'
+    styleUrl: './sprint-list.component.css',
 })
 export class SprintListComponent {
-
+    
+    sprintList = [
+        {
+            title : "Sprint 1 : Todo App",
+            description: "Todo App을 만들며 프론트엔드 개발 기초 스프린트를 실습합니다.",
+            url: "/todo-list",
+        }
+    ]
 }
