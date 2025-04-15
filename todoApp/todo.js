45e6286550eb016f19ea173dedc4bf15f7e5d713
hw <hyeongu0305@gmail.com>
2025-04-15 11:38:47 +0900

    할 일 생성 시 스토리지 저장하도록 수정

--- todoApp/todo.js
+++ todoApp/todo.js
@@ -1,5 +1,5 @@
-// 할 일 생성
-function createTodo() {
+// 할 일 추가 (new)
+function addTodo() {
     let todoInput = document.getElementById('todo-input')
     let inputText = todoInput.value
 
@@ -7,6 +7,26 @@ function createTodo() {
         alert('할 일을 입력해 주세요!')
         return false
     }
+
+    // 스토리지 저장
+    let todo = {
+        content: inputText
+    }
+    upsertTodo(todo)
+
+    // 할 일 생성
+    createTodo(inputText)
+}
+
+// 할 일 생성
+function createTodo(inputText) {
+    // let todoInput = document.getElementById('todo-input')
+    // let inputText = todoInput.value
+
+    // if (inputText == '') {
+    //     alert('할 일을 입력해 주세요!')
+    //     return false
+    // }
 
     let todoList = document.getElementById('todo-list')
     let newTodo = document.createElement('li')
@@ -55,7 +75,7 @@ function createTodo() {
     todoList.appendChild(newTodo)
 
     // 입력창 초기화
-    todoInput.value = ''
+    // todoInput.value = ''
 }
 
 // 할 일 삭제
@@ -97,4 +117,70 @@ function toggleComplete(checkbox) {
     // 현재 요소의 다음 형제 요소를 가져와라
     let textDiv = checkbox.nextElementSibling
     textDiv.classList.toggle('completed', checkbox.checked)
+}
+
+/** 
+ * 
+ * storage 관련 함수들
+*/
+// todo 리스트를 매개변수로 받아서 storage에 저장하는 함수
+function setTodoList(todoList) {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
+// 저장된 todo 리스트를 storage에서 가져오는 함수
+function getAllTodoList() {
+    if (localStorage.getItem('todoList') == null) {
+        return []
+    } else {
+        return JSON.parse(localStorage.getItem('todoList'))
+    }
+}
+
+// todo의 마지막 고유 id를 기록하는 함수
+function setLastId(lastId) {
+    localStorage.setItem('lastId', lastId)
+}
+
+// todo의 마지막 고유 id를 가져오는 함수
+function getLastId() {
+    if (localStorage.getItem('lastId') == null) {
+        return 0
+    } else {
+        JSON.parse(localStorage.getItem('lastId'))
+    }
+}
+
+// todo를 storage에 insert 하거나 update 하는 함수 : insert + update = upsert
+function upsertTodo(todo) {
+    // 각각의 todo에 고유한 id를 부여하기 위해 생성할 때마다 +1을 해준다
+    let id = getLastId() + 1;
+
+    // 만약 todo에 이미 id 값이 있다면 그 값으로 대체
+    if (todo['id'] != '') {
+        id = todo['id']
+    } else {
+
+    }
+
+    // 전체 todo 리스트를 가져옴
+    let todoList = getAllTodoList();
+
+    // 기존 todo 리스트에 같은 id를 가진 todo 가 존재한다면 새로 생성(insert)하는 것이 아닌 수정(update)을 진행
+    let todoIndex = todoList.findIndex(item => item.id == id);
+
+    if (todoIndex > -1) { // 같은 id가 이미 존재하는 경우 수정(update)
+        todoList[todoIndex] = todo;
+    } else { // 같은 id가 없는 경우 생성(insert)
+        todo['id'] = id
+        todoList.push(todo);
+    }
+
+    // 수정된 todo 리스트를 storage에 반영
+    setTodoList(todoList);
+
+    // 새롭게 생성일 경우에만, 생성이 성공적으로 완료 되었다면 현시점의 lastId를 저장 해준다.
+    if (todoIndex > -1) {
+        setLastId(id)
+    }
 }
